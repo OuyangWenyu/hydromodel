b91d2c91f23f9500d8989500b0062480dda374fc
OuyangWenyu <hust2014owen@gmail.com>
2022-06-28 15:02:42 +0800

    add some files (#17)
    Co-authored-by: forestbat <k678900000@163.com>

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name="",  # 输入项目名称
+    version="",  # 输入版本号
+    keywords=[""],  # 输入关键词
+    description="",  # 输入概述
+    long_description="",  # 输入描述
+
+    url="",  # 输入项目Github仓库的链接
+    author="",  # 输入作者名字
+    author_email="",  # 输入作者邮箱
+    license="",  # 此为声明文件，一般填写 MIT_license
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[""],  # 输入项目所用的包
+    python_requires='>= ',  # Python版本要求
+)

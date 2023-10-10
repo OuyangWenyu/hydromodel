c12830c6b56fd2bc1484eb07665d55dffcd7480b
forestbat <k678900000@163.com>
2023-10-10 16:17:22 +0800

    update setup.py to add long_description

--- setup.py
+++ setup.py
@@ -13,12 +13,10 @@ setup(
     description="",  # 输入概述
     long_description=readme,
     long_description_content_type="text/markdown",
-
     url="https://github.com/iHeadWater/hydro-model-xaj",  # 输入项目Github仓库的链接
     author="iHeadWater",  # 输入作者名字
     author_email="",  # 输入作者邮箱
     license="MIT_license",  # 此为声明文件，一般填写 MIT_license
-
     packages=find_packages(),
     include_package_data=True,
     platforms="any",

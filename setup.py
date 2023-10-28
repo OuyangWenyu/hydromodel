3caac4a9e2aa14f3db20c9c24264ebd84fcf59e0
ouyangwenyu <wenyuouyang@outlook.com>
2023-10-28 17:41:05 +0800

    add files from cookiecutter template

--- setup.py
+++ setup.py
@@ -1,30 +1,71 @@
+#!/usr/bin/env python
 """
 Author: Wenyu Ouyang
 Date: 2023-10-28 09:16:46
-LastEditTime: 2023-10-28 09:27:22
+LastEditTime: 2023-10-28 17:36:12
 LastEditors: Wenyu Ouyang
-Description: setup.py for hydromodel package
+Description: The setup script
 FilePath: \hydro-model-xaj\setup.py
 Copyright (c) 2023-2024 Wenyu Ouyang. All rights reserved.
 """
+import io
 import pathlib
+from os import path as op
 from setuptools import setup, find_packages
 
-readme = pathlib.Path("README.md").read_text()
+readme = pathlib.Path("README.md").read_text(encoding='utf-8')
+here = op.abspath(op.dirname(__file__))
+
+# get the dependencies and installs
+with io.open(op.join(here, "requirements.txt"), encoding="utf-8") as f:
+    all_reqs = f.read().split("\n")
+
+install_requires = [x.strip() for x in all_reqs if "git+" not in x]
+dependency_links = [x.strip().replace("git+", "") for x in all_reqs if "git+" not in x]
+
+requirements = []
+
+setup_requirements = [
+    "pytest-runner",
+]
+
+test_requirements = [
+    "pytest>=3",
+]
+
 setup(
-    name="hydromodel",  # 输入项目名称
-    version="0.0.1",  # 输入版本号
-    keywords=[""],  # 输入关键词
-    description="",  # 输入概述
+    author="Wenyu Ouyang",
+    author_email="wenyuouyang@outlook.com",
+    python_requires=">=3.9",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    description="hydrological models starting from XinAnJiang",
+    entry_points={
+        "console_scripts": [
+            "hydromodel=hydromodel.cli:main",
+        ],
+    },
+    install_requires=install_requires,
+    dependency_links=dependency_links,
+    license="GNU General Public License v3",
     long_description=readme,
     long_description_content_type="text/markdown",
-    url="https://github.com/iHeadWater/hydro-model-xaj",  # 输入项目Github仓库的链接
-    author="iHeadWater",  # 输入作者名字
-    author_email="",  # 输入作者邮箱
-    license="MIT_license",  # 此为声明文件，一般填写 MIT_license
-    packages=find_packages(),
     include_package_data=True,
-    platforms="any",
-    install_requires=[""],  # 输入项目所用的包
-    python_requires=">= 3.7 ",  # Python版本要求
+    keywords="hydromodel",
+    name="hydromodel",
+    packages=find_packages(include=["hydromodel", "hydromodel.*"]),
+    setup_requires=setup_requirements,
+    test_suite="tests",
+    tests_require=test_requirements,
+    url="https://github.com/iHeadWater/hydro-model-xaj",
+    version="0.0.1",
+    zip_safe=False,
 )

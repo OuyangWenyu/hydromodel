894f22527e5278537db8d52c8f5b92914587d7a3
ouyangwenyu <wenyuouyang@outlook.com>
2024-03-28 11:23:39 +0800

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -66,6 +66,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/iHeadWater/hydromodel",
-    version='0.1.0',
+    version='0.2.0',
     zip_safe=False,
 )

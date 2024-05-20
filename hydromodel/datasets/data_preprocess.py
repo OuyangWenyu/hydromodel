fix bug for prcp units like mm/6h

hydromodel/datasets/data_preprocess.py
@@ -1,10 +1,10 @@
 """
 Author: Wenyu Ouyang
 Date: 2022-10-25 21:16:22
-LastEditTime: 2024-03-27 18:17:14
+LastEditTime: 2024-05-20 20:08:10
 LastEditors: Wenyu Ouyang
 Description: preprocess data for models in hydro-model-xaj
-FilePath: \hydro-model-xaj\hydromodel\datasets\data_preprocess.py
+FilePath: \hydromodel\hydromodel\datasets\data_preprocess.py
 Copyright (c) 2021-2022 Wenyu Ouyang. All rights reserved.
 """
 
@@ -460,7 +460,7 @@ def get_ts_from_diffsource(data_type, data_dir, periods, basin_ids):
         ts_data = camels.read_ts_xrdataset(
             basin_ids, periods, ["prcp", "PET", "streamflow"]
         )
-        # trans unit to mm/day
+        # trans unit to mm/time_interval
         qobs_ = ts_data[["streamflow"]]
         target_unit = ts_data["prcp"].attrs.get("units", "unknown")
         r_mmd = streamflow_unit_conv(qobs_, basin_area, target_unit=target_unit)
@@ -526,19 +526,36 @@ def cross_val_split_tsdata(
 
 def get_rr_events(rain, flow, basin_area):
     ureg = UnitRegistry()
-    # trans unit to mm/day
+    # trans unit to mm/time_interval
     flow_threshold = streamflow_unit_conv(
         np.array([100]) * ureg.m**3 / ureg.s,
         basin_area.isel(basin=0).to_array().to_numpy() * ureg.km**2,
-        target_unit="mm/h",
+        target_unit=flow.units,
     )
+    # 正则表达式匹配 mm/xh 和 mm/xd 格式
+    match = re.match(r"mm/(\d+)(h|d)", flow.units)
+
+    if match:
+        num, unit = match.groups()
+        num = int(num)
+        if unit == "h":
+            multiple = num
+        elif unit == "d":
+            multiple = num * 24
+        else:
+            raise ValueError(f"Unsupported unit: {unit}")
+    else:
+        raise ValueError(f"Invalid unit format: {flow.units}")
+
+    print(f"flow.units = { flow.units}, multiple = {multiple}")
     rr_events = {}
     for basin in basin_area.basin.values:
         rr_event = rainfall_runoff_event_identify(
             rain.sel(basin=basin).to_series(),
             flow.sel(basin=basin).to_series(),
-            multiple=1,
+            multiple=multiple,
             flow_threshold=flow_threshold[0],
+            rain_min=0.02 * multiple,
         )
         rr_events[basin] = rr_event
     return rr_events
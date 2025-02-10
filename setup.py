11d9b7a0a6faa01e8a9d01dc7f296073e6a7f702
owen <wenyuouyang@outlook.com>
2025-02-10 15:40:17 +0800

    Bump version: 0.2.9 → 0.2.10

--- setup.py
+++ setup.py
@@ -64,6 +64,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/OuyangWenyu/hydromodel",
-    version='0.2.9',
+    version='0.2.10',
     zip_safe=False,
 )

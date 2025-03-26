bf320108dec629825a6373719f248dfe21d6cde7
zhuanglaihong <zhuanglaihong@gmail.com>
2025-03-26 20:15:24 +0800

    formed gr code

--- scripts/prepare_data.py
+++ scripts/prepare_data.py
@@ -1,12 +1,12 @@
-'''
-Author: zhuanglaihong
+"""
+Author: Wenyu Ouyang
 Date: 2025-02-18 10:20:58
-LastEditTime: 2025-03-20 10:15:20
+LastEditTime: 2025-03-26 20:04:48
 LastEditors: zhuanglaihong
 Description: Script for preparing data
 FilePath: /zlh/hydromodel/scripts/prepare_data.py
 Copyright: Copyright (c) 2021-2024 zhuanglaihong. All rights reserved.
-'''
+"""
 
 from pathlib import Path
 import sys
@@ -21,13 +21,13 @@ from hydromodel.datasets.data_preprocess import process_and_save_data_as_nc
 
 def main(args):
     data_path = args.origin_data_dir
-    origin_data_scale = args.origin_data_scale
     target_data_scale = args.target_data_scale
 
-    if process_and_save_data_as_nc(data_path, origin_data_scale,target_data_scale, save_folder=data_path):
+    if process_and_save_data_as_nc(data_path, target_data_scale, save_folder=data_path):
         print("Data is ready!")
     else:
         print("Data format is incorrect! Please check the data.")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Prepare data.")
@@ -37,13 +37,6 @@ if __name__ == "__main__":
         help="Path to your hydrological data foler",
         default="/home/zlh/hydromodel/data/camels_11532500",
         # default="C:\\Users\\wenyu\\Downloads\\biliuhe",
-    )
-    parser.add_argument(
-        "--origin_data_scale",
-        type=str,
-        help="your origin data time scale",
-        default="D",
-        # default="D"or"h"
     )
     parser.add_argument(
         "--target_data_scale",
